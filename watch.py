bb8c721ed21cba0ce36163c061b6b044c27c92d6
RJ <rjseven007@gmail.com>
2019-11-20 05:17:21 +0000

    2019-11-20 | 05-17-21

--- watch.py
+++ watch.py
@@ -1,1 +1,21 @@
-# def wait_end_of_changes(target, timeout):
+from random import random
+import threading
+import time
+
+result = None
+
+def background_calculation():
+    # here goes some long calculation
+    time.sleep(random() * 5 * 60)
+
+    # when the calculation is done, the result is stored in a global variable
+    global result
+    result = 42
+
+def wait_end_of_changes(target, timeout):
+    thread = threading.Thread(target=background_calculation)
+    thread.start()
+
+    # TODO: wait here for the result to be available before continuing!
+
+    print('The result is', result)
